1c7e0c612e1d2e687eb6fd2e74410b7a0e7dbfda
Mulyu <mu0lyu@gmail.com>
2023-03-29 18:12:13 +0900

    fix alias bug

--- src/exporter/notion/ParagraphBlockGenerator.ts
+++ src/exporter/notion/ParagraphBlockGenerator.ts
@@ -17,8 +17,14 @@ export class ParagraphBlockGenerator {
    */
   static fromDictionary(dictionary: Dictionary): BlockObjectRequest {
     const descriptionContentBlock: BlockObjectRequest = { object: 'block', type: 'paragraph', paragraph: { rich_text: [{ type: 'text', text: { content: dictionary.description.value }}] } };
-    const aliasContentBlock: BlockObjectRequest = { object: 'block', type: 'bulleted_list_item', bulleted_list_item: { rich_text: dictionary.name.aliasNames.map((aliasName) => ({ type: 'text', text: { content: aliasName }})) } };
-    const aliasBlock: BlockObjectRequest = { object: 'block', type: 'paragraph', paragraph: { rich_text: [{ type: 'text', text: { content: '別名' }, annotations: { bold: true }}], children: [aliasContentBlock] } };
+    const aliasBlock: BlockObjectRequest = {
+      object: 'block',
+      type: 'paragraph',
+      paragraph: {
+        rich_text: [{ type: 'text', text: { content: '別名' }, annotations: { bold: true }}],
+        children: dictionary.name.aliasNames.map((name) => ({ object: 'block', type: 'bulleted_list_item', bulleted_list_item: { rich_text: [{ type: 'text', text: { content: name }}] } })),
+      }
+    };
     const developerContentBlock: BlockObjectRequest = { object: 'block', type: 'paragraph', paragraph: { rich_text: [{ type: 'text', text: { content: `${dictionary.targetFilePath} L${dictionary.targetLine + 1}` }, annotations: { code: true }}] } };
     const developerBlock: BlockObjectRequest = { object: 'block', type: 'paragraph', paragraph: { rich_text: [{ type: 'text', text: { content: '開発者向け情報' }, annotations: { bold: true }}], children: [developerContentBlock] } };
 
